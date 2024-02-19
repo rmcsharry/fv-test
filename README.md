1a4f389b034a0b5abd67cc8b8a63e5c0dea8de93
Orlando <75616282+orlando-fv@users.noreply.github.com>
2024-02-19 12:57:55 +0000

    Update to latest deps (#4)
    Co-authored-by: Orlando <orlando.groppo@gmail.com>

--- README.md
+++ README.md
@@ -4,17 +4,17 @@ Setup the project:
 
 Make sure you install all the dependencies (currently yarn, but you can opt-out) and start the solution in dev mode.
 
-There is a simple homepage with links to the tasks below.
+There is a simple homepage with links to the tasks below. You can keep the pages router, but if you prefer to switch to app router it will be positively scored.
 
 First Task:
 
-- in the "feed" page, show a centered column of posts (use https://jsonplaceholder.typicode.com/ to get the data) which are simple boxes with the title and body properties returned
-- for each post, fetch its relative comments and show the counter, or nothing if there are no comments
+- in the "feed" page, show a centered column of posts (use https://jsonplaceholder.typicode.com/posts to get the data) which are simple boxes with the title and body properties returned
+- for each post, fetch its relative comments (https://jsonplaceholder.typicode.com/posts/{id}/comments) and show the counter, or nothing if there are no comments
 - when clicking on the comment counter, the comments appear below it
 
 Second Task:
 
-- create a "vault" page, showing a responsive grid of square pictures (use https://jsonplaceholder.typicode.com/ to get the data) which are simple thumbnails
+- create a "vault" page, showing a responsive grid of square pictures (use https://jsonplaceholder.typicode.com/albums/{id}/photos to get the data) which are simple thumbnails
 - when clicking on a thumbnail, the fullscreen image opens
 
 Requirements:
@@ -22,10 +22,11 @@ Requirements:
 - You should use MUI5 components https://mui.com/material-ui/
 - You can create your own component if needed, style it with the default `styled` from MUI without installing other libraries
 
-Bonus points:
+Keep in mind these:
 
+- Semantic markup
 - SSR considerations
+- a11y considerations
 - Type the responses from the API calls
 - create meaningful tags in the head of each page
 - add the favicon stealing it from fanvue.com ;)
-- a11y considerations
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
       <main>
         <Box textAlign={"center"} my={8}>
           <Typography component="h1" variant="h3">
-            Fanvue&apos;s Frontend coding challenge
+            Fanvue&apos;s Frontend coding challenge 💻
           </Typography>
         </Box>
         <Grid container spacing={1}>
@@ -24,22 +24,19 @@ const Home: NextPage = () => {
               justifyContent: "center",
             }}
           >
-            <NextLink href="/feed" passHref>
-              <Link>
-                <Paper
-                  sx={{
-                    textAlign: "center",
-                    border: "1px solid grey",
-                    borderRadius: 5,
-                    p: 3,
-                  }}
-                  elevation={3}
-                >
-                  <h2>Go to Feed page</h2>
-                  <p>And start the first task</p>
-                </Paper>
-              </Link>
-            </NextLink>
+            <Link component={NextLink} href="/feed">
+              <Paper
+                sx={{
+                  textAlign: "center",
+                  borderRadius: 5,
+                  p: 3,
+                }}
+                elevation={3}
+              >
+                <h2>Go to Feed page</h2>
+                <p>And start the first task</p>
+              </Paper>
+            </Link>
           </Grid>
           <Grid
             item
@@ -49,22 +46,19 @@ const Home: NextPage = () => {
               justifyContent: "center",
             }}
           >
-            <NextLink href="/vault" passHref>
-              <Link>
-                <Paper
-                  sx={{
-                    textAlign: "center",
-                    border: "1px solid grey",
-                    borderRadius: 5,
-                    p: 3,
-                  }}
-                  elevation={3}
-                >
-                  <h2>Go to Vault page</h2>
-                  <p>And start the second task</p>
-                </Paper>
-              </Link>
-            </NextLink>
+            <Link component={NextLink} href="/vault">
+              <Paper
+                sx={{
+                  textAlign: "center",
+                  borderRadius: 5,
+                  p: 3,
+                }}
+                elevation={3}
+              >
+                <h2>Go to Vault page</h2>
+                <p>And start the second task</p>
+              </Paper>
+            </Link>
           </Grid>
         </Grid>
       </main>
