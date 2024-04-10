7b25848a304bfa8d572d88879a46794e7e6331b7
Richard McSharry <rmcsharry@gmail.com>
2024-04-10 17:27:53 +0200

    add fetching comments, and display a count

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,22 +1,46 @@
 import { Box, Paper, Typography } from '@mui/material';
 
+type Comment =   {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
+
 type PostsRepository = {
   userId: number;
   id: number;
   title: string;
   body: string;
+  commentsCount?: number;
+  comments?: Comment[];
 }
+
 
 export async function getPosts() {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   if (!res.ok) throw new Error("Failed to fetch");
 
-  return res.json();
+  const posts: PostsRepository[] = await res.json();
+
+  const postsWithComments = await Promise.all(posts.map(async (post) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments`);
+    const comments = await res.json();
+    return { ...post, commentsCount: comments.length };
+  }));
+
+  return postsWithComments;
+}
+
+export const metadata = {
+  title: 'Example Feed of Posts',
+  description: 'This is an example feed of posts returned from jsonplaceholder.typicode.com',
 }
 
 const Feed = async () => {
   const posts: PostsRepository[] = await getPosts();
-  console.log(posts);
+  
   return (
     <main>
       {posts?.length === 0 && <h1>Loading...</h1>}
@@ -25,6 +49,7 @@ const Feed = async () => {
           <Paper sx={{padding: 2}}>
             <Typography variant="h5">{post.title}</Typography>
             <Typography>{post.body}</Typography>
+            {post.commentsCount && post.commentsCount > 0 && <Typography>{post.commentsCount} comments</Typography>}
           </Paper>
         </Box>
       ))}
