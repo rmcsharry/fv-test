667247611fcc11d89c6ac18399b9ec09a1ea72be
Richard McSharry <rmcsharry@gmail.com>
2024-04-10 16:44:46 +0200

    fix favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,7 @@
-import Head from 'next/head'
 import ClientLayout from './ClientLayout'
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <body>
         <ClientLayout>{children}</ClientLayout>
       </body>
